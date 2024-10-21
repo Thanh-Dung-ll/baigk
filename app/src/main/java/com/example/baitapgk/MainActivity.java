ef5f7201657e73f35c84869c6e27e173aac4168d
dung.github.io <22115141122103@.sv.ute.udn.vn>
2024-10-21 08:38:25 +0700

    1

---
+++ app/src/main/java/com/example/baitapgk/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.baitapgk;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button savenote;
+    Button Displaynote;
+    EditText nhapnote;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+    }
+}
